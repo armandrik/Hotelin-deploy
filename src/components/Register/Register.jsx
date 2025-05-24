9b5ea82bad9f850de4d2c7290affe997e88a0bbf
Arman Drik <arman.driik@gmail.com>
2025-05-24 16:10:19 +0330

    fix form logic

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,7 @@ import { Login } from "./Login/Login";
 import { Signup } from "./Signup/Signup";
 
 export const Register = () => {
-  const [formType, setFormType] = useState("login");  
+  const [formType, setFormType] = useState("login");
 
   return (
     <div className="h-dvh px-6 flex flex-col items-center justify-center">
@@ -11,8 +11,8 @@ export const Register = () => {
       <h1 className="mb-11 text-h-light text-3xl font-medium dark:text-h-dark">
         {formType === "login" ? "Login" : "Sign up"}
       </h1>
-      {formType ? <Login /> : <Signup />}
-      {formType ? (
+      {formType === "login" ? <Login /> : <Signup />}
+      {formType === "login" ? (
         <p className="mb-10 text-sm text-p-light text-left w-full">
           Dont you have an account?{" "}
           <span
@@ -34,7 +34,7 @@ export const Register = () => {
         </p>
       )}
       <p className="mb-4 text-p-light text-sm dark:text-p-dark">
-        Or {formType ? "Login" : "Sign up"} with google account
+        Or {formType === "login" ? "Login" : "Sign up"} with google account
       </p>
       <div className="w-full pb-2.5 pt-4 bg-white flex items-center justify-center rounded-lg shadow-xs cursor-pointer dark:bg-secondary-dark">
         <svg width="32" height="32">
