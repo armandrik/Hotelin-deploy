b060bdc101892647696d100756b17298d84bd1ec
Arman Drik <arman.driik@gmail.com>
2025-05-28 16:45:01 +0330

    sadf

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useEffect, useState } from "react";
-import {AppRoutes} from "src/components";
-import {SplashScreen} from "src/components";
+import { AppRoutes } from "src/components";
+import { SplashScreen } from "src/components";
 
 function App() {
   const [isLoading, setIsLoading] = useState(true);
 
+  const isPWAStandalone = () =>
+    window.matchMedia("(display-mode: standalone)").matches ||
+    window.navigator.standalone === true;
+
   useEffect(() => {
+    // If in standalone (PWA mode), skip splash
+    if (isPWAStandalone()) {
+      setIsLoading(false);
+      return;
+    }
+
     const alreadyShown = sessionStorage.getItem("splashShown") === "true";
 
     if (alreadyShown) {
